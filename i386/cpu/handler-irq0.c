8e29cc7f8b6514c0cef96008b12308596f153c0a
icarotelesdasilva <telesicaro04@gmail.com>
2026-07-30 23:02:03 -0300

    emergency atualization

--- i386/cpu/handler-irq0.c
+++
@@ -1,8 +0,0 @@
-#include "../interrupts/io.h"
-void irq0_handler(void) {
-
-// This handler was placed for when it enabled the interruptions irq 0 sent it to idt but idt did not know what to do: (emergency update).*/
-
-outb(0x20, 0x20); // Send EOI to PIC
-
-}
