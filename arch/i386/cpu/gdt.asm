ea47638800bb209261a8cf8839ecc2e1e511cfe7
Pão <telesicaro04@gmail.com>
2026-08-01 13:44:08 -0300

    Atualizar o gdt.asm

--- arch/i386/cpu/gdt.asm
+++ arch/i386/cpu/gdt.asm
@@ -2,15 +2,15 @@ global gdt_flush
 gdt_flush:
 MOV eax, [esp + 4]
 lgdt [eax]
-mov ax, 0x10
-    mov ds, ax
-    mov es, ax
-    mov fs, ax
-    mov gs, ax
-    mov ss, ax
+MOV ax, 0x10
+    MOV ds, ax
+    MOV es, ax
+    MOV fs, ax
+    MOV gs, ax
+    MOV ss, ax
 
     jmp 0x08:.reload_cs
 
     .reload_cs:
-    ret
+    RET
     
