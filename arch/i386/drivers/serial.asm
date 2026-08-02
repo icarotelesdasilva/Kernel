f6692bc9d89e14dbd378edbcd58587456ec29a41
Pão <telesicaro04@gmail.com>
2026-08-02 13:35:57 -0300

    Atualizar o serial.asm

--- arch/i386/drivers/serial.asm
+++ arch/i386/drivers/serial.asm
@@ -5,36 +5,38 @@ COM1_PORT equ 0x3F8
 global init_serial
 init_serial:
     ; clean interrupts
-    mov dx, COM1_PORT + 1
-    mov al, 0x00
-    out dx, al
+    MOV dx, COM1_PORT + 1
+    MOV al, 0x00
+    OUT dx, al
 
     ; enable dlab
-    mov dx, COM1_PORT + 3
-    mov al, 0x80
-    out dx, al
+    MOV dx, COM1_PORT + 3
+    MOV al, 0x80
+    OUT dx, al
 
     ; define speed 115200
-    mov dx, COM1_PORT + 0
-    mov al, 0x01
-    out dx, al
-    mov dx, COM1_PORT + 1
-    mov al, 0x00
-    out dx, al
+    MOV dx, COM1_PORT + 0
+    MOV al, 0x01
+    OUT dx, al
+    MOV dx, COM1_PORT + 1
+    MOV al, 0x00
+    OUT dx, al
 
     ; LCR
-    mov dx, COM1_PORT + 3
-    mov al, 0x03
-    out dx, al
+    MOV dx, COM1_PORT + 3
+    MOV al, 0x03
+    MOV dx, al
 
     ; FIFO
-    mov dx, COM1_PORT + 2
-    mov al, 0x03
-    out dx, al
+    MOV dx, COM1_PORT + 2
+    MOV al, 0x03
+    OUT dx, al
 
     ; MCR
-    mov dx, COM1_PORT + 4
-    mov al, 0x08
-    out dx, al
+    MOV dx, COM1_PORT + 4
+    MOV al, 0x08
+    OUT dx, al
 
-    ret
+    RET
+
+section .note.GNU-stack noalloc noexec nowrite progbits
