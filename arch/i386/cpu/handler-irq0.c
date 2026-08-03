51538acaecad73e96e8850a17b87a052e646600d
Pão <telesicaro04@gmail.com>
2026-08-02 22:33:05 -0300

    Atualizar o handler-irq0.c

--- arch/i386/cpu/handler-irq0.c
+++ arch/i386/cpu/handler-irq0.c
@@ -2,7 +2,12 @@
 #include "../interrupts/pic.h"
 void irq0_handler(void) {
 
-// This handler was placed for when it enabled the interruptions irq 0 sent it to idt but idt did not know what to do: (emergency update).*/
+/* 
+ * Emergency Update:
+ * This handler was placed for when it enabled the interruptions 
+ * IRQ 0 sent it to IDT, but IDT did not know what to do.
+ */
+
 
  // Send EOI to PIC
  pic_send_eoi(0); 
